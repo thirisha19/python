e739bdb2a1161e2c7edce97dbb75656e07fdaa9c
root <root@localhost>
2022-05-26 10:12:43 +0530

    auto publish 1653540163130

--- src/divzpgm/VowelCount.py
+++ src/divzpgm/VowelCount.py
@@ -1,14 +1,16 @@
 
-str=input("enter a string")
-print(str)
+str=input("enter a string: ")
+#print(str)
 index=len(str)
 vowel_count= 0
-print(index) 
-for i in range(index):
-  if str[i] in ('a','e','i','o','u','A','E','i','o','u'):
+#print(index) 
+for i in range(index): 
+  if str[i] in ('a','e','i','o','u','A','E','i','o','u'): 
     vowel_count+=1
+ # print("value of i is {} and str value of i is {} and vowel count is : {} ".format(i,str[i],vowel_count))
 print("total number of vowel is :", vowel_count)
 
   
 
 
+ 
